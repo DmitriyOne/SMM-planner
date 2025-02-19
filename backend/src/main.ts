2190a05482185fcf90f4d20a7eb0a5488e22a7d1
DmitriyOne <senkoner4@gmail.com>
2025-02-19 21:29:53 +0100

    Add global prefix & logger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PREFIX } from './constants/prefix.constant';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const logger = new Logger('Bootstrap');
 
   app.enableShutdownHooks();
 
-  await app.listen(process.env.PORT ?? 5001);
+  app.setGlobalPrefix(PREFIX.getGlobal());
+
+  const port = process.env.PORT ?? 5001;
+  await app.listen(port);
+
+  logger.log(`Server is running on port: ${port}`);
 }
 
 bootstrap();
