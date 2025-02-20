1efd58be0e98614b05f2eb0072c907650e8aee57
DmitriyOne <senkoner4@gmail.com>
2025-02-20 10:25:41 +0100

    Create post entity

--- backend/src/posts/entities/post.entity.ts
+++ backend/src/posts/entities/post.entity.ts
@@ -1,1 +1,28 @@
-export class Post {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Post } from '@prisma/client';
+
+export class PostEntity implements Post {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty()
+  description: string;
+
+  @ApiProperty({ required: false, nullable: true })
+  image: string;
+
+  @ApiProperty()
+  isPublish: boolean;
+
+  @ApiProperty({ required: false, nullable: true })
+  isApproved: boolean;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
