09a8880f637ba3e1d40951a5505c789732c78470
DmitriyOne <senkoner4@gmail.com>
2025-02-20 12:08:50 +0100

    Add api-response for swagger

--- backend/src/posts/posts.controller.ts
+++ backend/src/posts/posts.controller.ts
@@ -4,40 +4,47 @@ import { CreatePostDto } from './dto/create-post.dto'
 import { UpdatePostDto } from './dto/update-post.dto'
 import { PREFIX } from 'src/constants/prefix.constant'
 import { FindPostsDto } from './dto/find-posts.dto'
-import { ApiOkResponse } from '@nestjs/swagger'
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger'
 import { PostEntity } from './entities/post.entity'
+import { DeletePostResponseDto } from './dto/delete-post-response.dto.ts'
+import { capitalizeFirstLetter } from 'src/utils/string.utils'
 
 @Controller(PREFIX.POSTS)
+@ApiTags(capitalizeFirstLetter(PREFIX.POSTS))
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @HttpCode(200)
-  @ApiOkResponse({ type: PostEntity, isArray: true })
   @Post()
+  @ApiOkResponse({ type: PostEntity, isArray: true })
   findAll(@Body() findPostsDto: FindPostsDto) {
     return this.postsService.findAll(findPostsDto)
   }
 
   @Post(PREFIX.CREATE)
+  @ApiOkResponse({ type: CreatePostDto })
   create(@Body() createPostDto: CreatePostDto) {
     return this.postsService.create(createPostDto)
   }
 
   @HttpCode(200)
   @Post(PREFIX.ID)
+  @ApiOkResponse({ type: PostEntity })
   findOne(@Param('id') id: string) {
     return this.postsService.findOne(+id)
   }
 
   @Patch(PREFIX.ID)
+  @ApiOkResponse({ type: PostEntity })
   update(@Param('id') id: string, @Body() updatePostDto: UpdatePostDto) {
     return this.postsService.update(+id, updatePostDto)
   }
 
   @Delete(PREFIX.ID)
-  async remove(@Param('id') id: string) {
+  @ApiOkResponse({ type: DeletePostResponseDto })
+  async remove(@Param('id') id: string): Promise<DeletePostResponseDto> {
     const deletedPost = await this.postsService.remove(+id)
 
-    return `Post ${deletedPost.id} was deleted`
+    return { message: `Post ${deletedPost.id} was deleted` }
   }
 }
