286c0ae420620d7e26a7d6744a12e65c2448d0e7
DmitriyOne <senkoner4@gmail.com>
2025-02-20 10:56:19 +0100

    Update eslint&prittier in backend

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: ['prettier', '@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
@@ -23,11 +23,11 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'off',
     '@typescript-eslint/no-unused-vars': 'warn',
     '@typescript-eslint/semi': ['warn', 'never'],
-    '@typescript-eslint/object-curly-spacing': ["warn", "always"],
-    '@typescript-eslint/space-before-blocks': ["warn", "always"],
-    '@typescript-eslint/quotes': ["warn", "single", { "avoidEscape": true, "allowTemplateLiterals": true }],
-    '@typescript-eslint/comma-dangle': ["warn", "always-multiline"],
-    '@typescript-eslint/comma-spacing': ["warn", { "before": false, "after": true }],
-    '@typescript-eslint/key-spacing': ["warn", { "afterColon": true }]
+    '@typescript-eslint/object-curly-spacing': ['warn', 'always'],
+    '@typescript-eslint/space-before-blocks': ['warn', 'always'],
+    '@typescript-eslint/quotes': ['warn', 'single', { 'avoidEscape': true, 'allowTemplateLiterals': true }],
+    '@typescript-eslint/comma-dangle': ['warn', 'always-multiline'],
+    '@typescript-eslint/comma-spacing': ['warn', { 'before': false, 'after': true }],
+    '@typescript-eslint/key-spacing': ['warn', { 'afterColon': true }],
   },
 };
