6409e41e327fe94dce3cc9242e51f21459443f54
DmitriyOne <senkoner4@gmail.com>
2025-02-20 12:25:25 +0100

    Add validation input data

--- backend/src/posts/dto/create-post.dto.ts
+++ backend/src/posts/dto/create-post.dto.ts
@@ -1,26 +1,39 @@
 import { ApiProperty } from '@nestjs/swagger'
 import { Post } from '@prisma/client'
+import { IsBoolean, IsNotEmpty, IsOptional, IsString, MinLength } from 'class-validator'
 
 type TCreatePost = Omit<Post, 'id' | 'createdAt' | 'updatedAt'>
 
 export class CreatePostDto implements TCreatePost {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   @ApiProperty({ default: 'Lorem ipsum' })
   title: string
 
+  @IsString()
+  @IsNotEmpty()
   @ApiProperty({
     default: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
   })
   description: string
 
+  @IsString()
+  @IsOptional()
+  @IsNotEmpty()
   @ApiProperty({
     required: false,
     default: 'https://res.cloudinary.com/due1q2azx/image/upload/v1739974186/samples/balloons.jpg',
   })
   image: string | null
 
+  @IsBoolean()
+  @IsOptional()
   @ApiProperty({ default: false })
   isPublish: boolean
 
+  @IsBoolean()
+  @IsOptional()
   @ApiProperty({ default: false })
   isApproved: boolean
 }
--- backend/src/posts/dto/find-posts.dto.ts
+++ backend/src/posts/dto/find-posts.dto.ts
@@ -1,9 +1,14 @@
 import { ApiProperty } from '@nestjs/swagger'
+import { IsBoolean, IsOptional } from 'class-validator'
 
 export class FindPostsDto {
+  @IsBoolean()
+  @IsOptional()
   @ApiProperty({ required: false, default: false })
   isPublish?: boolean = false
 
+  @IsBoolean()
+  @IsOptional()
   @ApiProperty({ required: false, default: false })
   isApproved?: boolean = false
 }
