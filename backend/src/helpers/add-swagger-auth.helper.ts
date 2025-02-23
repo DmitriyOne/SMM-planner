c4da94d1582e6d4fa255c478ee110e245416ac44
DmitriyOne <senkoner4@gmail.com>
2025-02-23 20:04:48 +0100

    Bad swagger auth

---
+++ backend/src/helpers/add-swagger-auth.helper.ts
@@ -0,0 +1,20 @@
+import { OpenAPIObject } from '@nestjs/swagger'
+import { ENDPOINT_PUBLIC } from 'src/constants/endpoint.constant'
+
+export const addSwaggerAuthToPrivateEndpoints = (document: OpenAPIObject) => {
+  Object.keys(document.paths).forEach((path) => {
+    const pathItem = document.paths[path]
+
+    Object.keys(pathItem).forEach((method) => {
+      const operation = pathItem[method]
+
+      const isPublic = Object.values(ENDPOINT_PUBLIC).some(
+        (endpoint) => endpoint.path === path && endpoint.method === method,
+      )
+
+      if (!isPublic) {
+        operation.security = [{ bearerAuth: [] }]
+      }
+    })
+  })
+}
