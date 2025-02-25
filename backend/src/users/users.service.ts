b3d723720a885293b52cd477665e825eda6c4790
DmitriyOne <senkoner4@gmail.com>
2025-02-25 12:06:44 +0100

    Remove unused import

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
 import { PrismaService } from 'src/prisma/prisma.service'
-import { AuthService } from 'src/auth/auth.service'
 import * as bcrypt from 'bcrypt'
 
 @Injectable()
