741fe8084ee6acb6147a050d4920bb18358d97d1
DmitriyOne <senkoner4@gmail.com>
2025-02-25 09:52:19 +0100

    Add constants

---
+++ backend/src/constants/main.constant.ts
@@ -0,0 +1,6 @@
+export const MAIN_LOGGER_NAME = 'Bootstrap'
+export const MAIN_SERVER_RUNNING_MSG = (port: number | string) => `Server is running on port: ${port}`
+export const MAIN_APPLICATION_RUNNING_MSG = (url: string) => `Application is running on: ${url}`
+
+export const MAIN_SWAGGER_TITLE = 'SMM-planner'
+export const MAIN_SWAGGER_VERSION = '1.0'
