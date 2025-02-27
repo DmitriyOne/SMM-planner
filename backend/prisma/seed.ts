e1a6f89e76059f148b1f08229f603eeea37783fb
DmitriyOne <senkoner4@gmail.com>
2025-02-27 10:23:27 +0100

    Add relation "PostTags" & remove postId

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,120 +1,173 @@
-import { PrismaClient } from '@prisma/client'
+import { Post, PrismaClient, Tag, User } from '@prisma/client'
 import * as bcrypt from 'bcrypt'
 
 const prisma = new PrismaClient()
 const roundsOfHashing = 10
 
 async function main() {
-  const passwordUser1 = await bcrypt.hash('123456', roundsOfHashing)
-  const passwordUser2 = await bcrypt.hash('654321', roundsOfHashing)
+  const [passwordUser1, passwordUser2] = await Promise.all([
+    bcrypt.hash('123456', roundsOfHashing),
+    bcrypt.hash('654321', roundsOfHashing),
+  ])
 
-  const user1 = await prisma.user.upsert({
-    where: { email: 'user1@gmail.com' },
-    update: {
-      password: passwordUser1,
-    },
-    create: {
+  const usersData: Partial<User>[] = [
+    {
       email: 'user1@gmail.com',
-      name: 'User 1',
       password: passwordUser1,
+      name: 'User 1',
       role: 'admin',
     },
-  })
-  const user2 = await prisma.user.upsert({
-    where: { email: 'user2@gmail.com' },
-    update: {
-      password: passwordUser2,
-    },
-    create: {
+    {
       email: 'user2@gmail.com',
-      name: 'User 2',
       password: passwordUser2,
-      role: 'editor',
+      name: 'User 2',
+      role: 'reader',
     },
-  })
+  ]
 
-  const post1 = await prisma.post.upsert({
-    where: { title: 'Getting Started with Prisma: Why It’s Worth Learning' },
-    update: {
-      authorId: user2.id,
-    },
-    create: {
+  const postsData: Partial<Post>[] = [
+    {
       title: 'Getting Started with Prisma: Why It’s Worth Learning',
       description:
         'I’m diving into Prisma for the first time, and it’s already proving to be a powerful tool for database management. Here are my first impressions!',
       image: 'https://res.cloudinary.com/due1q2azx/image/upload/v1739983588/smm-planner/prisma.jpg',
       isApproved: false,
       isPublish: false,
-      authorId: user2.id,
     },
-  })
-
-  const post2 = await prisma.post.upsert({
-    where: { title: 'What’s Next? Expanding Your Development Skills' },
-    update: {
-      authorId: user1.id,
-    },
-    create: {
+    {
       title: 'What’s Next? Expanding Your Development Skills',
       description:
         'After mastering backend development, mobile development could be your next challenge. React Native or Flutter might open exciting new opportunities!',
       image: 'https://res.cloudinary.com/due1q2azx/image/upload/v1739983588/smm-planner/react-native-vs-flutter.jpg',
       isApproved: true,
       isPublish: false,
-      authorId: user1.id,
     },
-  })
-
-  const post3 = await prisma.post.upsert({
-    where: { title: 'The Key to Success in IT: Never Stop Learning' },
-    update: {
-      authorId: user2.id,
-    },
-    create: {
+    {
       title: 'The Key to Success in IT: Never Stop Learning',
       description:
         'Technology evolves fast, and staying ahead means constantly improving your skills. The more you learn, the more opportunities you unlock!',
       image: 'https://res.cloudinary.com/due1q2azx/image/upload/v1739983588/smm-planner/never-stop-learning.jpg',
       isApproved: true,
       isPublish: true,
+    },
+  ]
+
+  const tagsData: Partial<Tag>[] = [{ title: 'Frontend' }, { title: 'Backend' }, { title: 'Mobile' }]
+
+  const user1 = await prisma.user.upsert({
+    where: { email: usersData[0].email },
+    update: {
+      password: usersData[0].password,
+    },
+    create: {
+      email: usersData[0].email,
+      name: usersData[0].name,
+      password: usersData[0].password,
+      role: usersData[0].role,
+    },
+  })
+  const user2 = await prisma.user.upsert({
+    where: { email: usersData[1].email },
+    update: {
+      password: usersData[1].password,
+    },
+    create: {
+      email: usersData[1].email,
+      name: usersData[1].name,
+      password: usersData[1].password,
+      role: usersData[1].role,
+    },
+  })
+
+  const post1 = await prisma.post.upsert({
+    where: { title: postsData[0].title },
+    update: {
+      authorId: user1.id,
+    },
+    create: {
+      title: postsData[0].title,
+      description: postsData[0].description,
+      image: postsData[0].image,
+      isApproved: postsData[0].isApproved,
+      isPublish: postsData[0].isPublish,
+      authorId: user1.id,
+    },
+  })
+  const post2 = await prisma.post.upsert({
+    where: { title: postsData[1].title },
+    update: {
+      authorId: user2.id,
+    },
+    create: {
+      title: postsData[1].title,
+      description: postsData[1].description,
+      image: postsData[1].image,
+      isApproved: postsData[1].isApproved,
+      isPublish: postsData[1].isPublish,
+      authorId: user2.id,
+    },
+  })
+  const post3 = await prisma.post.upsert({
+    where: { title: postsData[2].title },
+    update: {
+      authorId: user2.id,
+    },
+    create: {
+      title: postsData[2].title,
+      description: postsData[2].description,
+      image: postsData[2].image,
+      isApproved: postsData[2].isApproved,
+      isPublish: postsData[2].isPublish,
       authorId: user2.id,
     },
   })
 
   const tag1 = await prisma.tag.upsert({
-    where: { title: 'Frontend' },
+    where: { title: tagsData[0].title },
     update: {
       authorId: user1.id,
-      postId: post1.id,
+      posts: {
+        connect: { id: post1.id },
+      },
     },
     create: {
-      title: 'Frontend',
+      title: tagsData[0].title,
       authorId: user1.id,
-      postId: post1.id,
+      posts: {
+        connect: { id: post1.id },
+      },
     },
   })
   const tag2 = await prisma.tag.upsert({
-    where: { title: 'Backend' },
+    where: { title: tagsData[1].title },
     update: {
       authorId: user2.id,
-      postId: post2.id,
+      posts: {
+        connect: { id: post2.id },
+      },
     },
     create: {
-      title: 'Backend',
+      title: tagsData[1].title,
       authorId: user2.id,
-      postId: post2.id,
+      posts: {
+        connect: { id: post2.id },
+      },
     },
   })
   const tag3 = await prisma.tag.upsert({
-    where: { title: 'General' },
+    where: { title: tagsData[2].title },
     update: {
-      authorId: user1.id,
-      postId: post1.id,
+      authorId: user2.id,
+      posts: {
+        connect: { id: post3.id },
+      },
     },
     create: {
-      title: 'General',
-      authorId: user1.id,
-      postId: post1.id,
+      title: tagsData[2].title,
+      authorId: user2.id,
+      posts: {
+        connect: [{ id: post3.id }, { id: post2.id }],
+      },
     },
   })
 
