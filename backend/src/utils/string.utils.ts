7869e5de6495ded9cfd51a921b8e93499a2aa79d
DmitriyOne <senkoner4@gmail.com>
2025-02-27 21:30:42 +0100

    Create "toUpperCaseString" until

--- backend/src/utils/string.utils.ts
+++ backend/src/utils/string.utils.ts
@@ -3,3 +3,9 @@ export const capitalizeFirstLetter = (str: string): string => {
 
   return str.charAt(0).toUpperCase() + str.slice(1)
 }
+
+export const toUpperCaseString = (str: string): string => {
+  if (!str) return str
+
+  return str.toUpperCase()
+}
