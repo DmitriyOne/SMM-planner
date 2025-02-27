3fe5f092cbc903d484e95f88049ce551c0a31ae0
DmitriyOne <senkoner4@gmail.com>
2025-02-27 16:49:07 +0100

    Add export seed data

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -4,6 +4,50 @@ import * as bcrypt from 'bcrypt'
 const prisma = new PrismaClient()
 const roundsOfHashing = 10
 
+export const usersSeedData: Partial<User>[] = [
+  {
+    email: 'user1@gmail.com',
+    password: '123456',
+    name: 'User 1',
+    role: 'admin',
+  },
+  {
+    email: 'user2@gmail.com',
+    password: '123456',
+    name: 'User 2',
+    role: 'reader',
+  },
+]
+
+export const postsSeedData: Partial<Post>[] = [
+  {
+    title: 'Getting Started with Prisma: Why It’s Worth Learning',
+    description:
+      'I’m diving into Prisma for the first time, and it’s already proving to be a powerful tool for database management. Here are my first impressions!',
+    image: 'https://res.cloudinary.com/due1q2azx/image/upload/v1739983588/smm-planner/prisma.jpg',
+    isApproved: false,
+    isPublish: false,
+  },
+  {
+    title: 'What’s Next? Expanding Your Development Skills',
+    description:
+      'After mastering backend development, mobile development could be your next challenge. React Native or Flutter might open exciting new opportunities!',
+    image: 'https://res.cloudinary.com/due1q2azx/image/upload/v1739983588/smm-planner/react-native-vs-flutter.jpg',
+    isApproved: true,
+    isPublish: false,
+  },
+  {
+    title: 'The Key to Success in IT: Never Stop Learning',
+    description:
+      'Technology evolves fast, and staying ahead means constantly improving your skills. The more you learn, the more opportunities you unlock!',
+    image: 'https://res.cloudinary.com/due1q2azx/image/upload/v1739983588/smm-planner/never-stop-learning.jpg',
+    isApproved: true,
+    isPublish: true,
+  },
+]
+
+export const tagsSeedData: Partial<Tag>[] = [{ title: 'Frontend' }, { title: 'Backend' }, { title: 'Mobile' }]
+
 async function main() {
   const [passwordUser1, passwordUser2] = await Promise.all([
     bcrypt.hash('123456', roundsOfHashing),
@@ -12,47 +56,14 @@ async function main() {
 
   const usersData: Partial<User>[] = [
     {
-      email: 'user1@gmail.com',
+      ...usersSeedData[0],
       password: passwordUser1,
-      name: 'User 1',
-      role: 'admin',
     },
     {
-      email: 'user2@gmail.com',
+      ...usersSeedData[1],
       password: passwordUser2,
-      name: 'User 2',
-      role: 'reader',
     },
   ]
-
-  const postsData: Partial<Post>[] = [
-    {
-      title: 'Getting Started with Prisma: Why It’s Worth Learning',
-      description:
-        'I’m diving into Prisma for the first time, and it’s already proving to be a powerful tool for database management. Here are my first impressions!',
-      image: 'https://res.cloudinary.com/due1q2azx/image/upload/v1739983588/smm-planner/prisma.jpg',
-      isApproved: false,
-      isPublish: false,
-    },
-    {
-      title: 'What’s Next? Expanding Your Development Skills',
-      description:
-        'After mastering backend development, mobile development could be your next challenge. React Native or Flutter might open exciting new opportunities!',
-      image: 'https://res.cloudinary.com/due1q2azx/image/upload/v1739983588/smm-planner/react-native-vs-flutter.jpg',
-      isApproved: true,
-      isPublish: false,
-    },
-    {
-      title: 'The Key to Success in IT: Never Stop Learning',
-      description:
-        'Technology evolves fast, and staying ahead means constantly improving your skills. The more you learn, the more opportunities you unlock!',
-      image: 'https://res.cloudinary.com/due1q2azx/image/upload/v1739983588/smm-planner/never-stop-learning.jpg',
-      isApproved: true,
-      isPublish: true,
-    },
-  ]
-
-  const tagsData: Partial<Tag>[] = [{ title: 'Frontend' }, { title: 'Backend' }, { title: 'Mobile' }]
 
   const user1 = await prisma.user.upsert({
     where: { email: usersData[0].email },
@@ -80,50 +91,50 @@ async function main() {
   })
 
   const post1 = await prisma.post.upsert({
-    where: { title: postsData[0].title },
+    where: { title: postsSeedData[0].title },
     update: {
       authorId: user1.id,
     },
     create: {
-      title: postsData[0].title,
-      description: postsData[0].description,
-      image: postsData[0].image,
-      isApproved: postsData[0].isApproved,
-      isPublish: postsData[0].isPublish,
+      title: postsSeedData[0].title,
+      description: postsSeedData[0].description,
+      image: postsSeedData[0].image,
+      isApproved: postsSeedData[0].isApproved,
+      isPublish: postsSeedData[0].isPublish,
       authorId: user1.id,
     },
   })
   const post2 = await prisma.post.upsert({
-    where: { title: postsData[1].title },
+    where: { title: postsSeedData[1].title },
     update: {
       authorId: user2.id,
     },
     create: {
-      title: postsData[1].title,
-      description: postsData[1].description,
-      image: postsData[1].image,
-      isApproved: postsData[1].isApproved,
-      isPublish: postsData[1].isPublish,
+      title: postsSeedData[1].title,
+      description: postsSeedData[1].description,
+      image: postsSeedData[1].image,
+      isApproved: postsSeedData[1].isApproved,
+      isPublish: postsSeedData[1].isPublish,
       authorId: user2.id,
     },
   })
   const post3 = await prisma.post.upsert({
-    where: { title: postsData[2].title },
+    where: { title: postsSeedData[2].title },
     update: {
       authorId: user2.id,
     },
     create: {
-      title: postsData[2].title,
-      description: postsData[2].description,
-      image: postsData[2].image,
-      isApproved: postsData[2].isApproved,
-      isPublish: postsData[2].isPublish,
+      title: postsSeedData[2].title,
+      description: postsSeedData[2].description,
+      image: postsSeedData[2].image,
+      isApproved: postsSeedData[2].isApproved,
+      isPublish: postsSeedData[2].isPublish,
       authorId: user2.id,
     },
   })
 
   const tag1 = await prisma.tag.upsert({
-    where: { title: tagsData[0].title },
+    where: { title: tagsSeedData[0].title },
     update: {
       authorId: user1.id,
       posts: {
@@ -131,7 +142,7 @@ async function main() {
       },
     },
     create: {
-      title: tagsData[0].title,
+      title: tagsSeedData[0].title,
       authorId: user1.id,
       posts: {
         connect: { id: post1.id },
@@ -139,7 +150,7 @@ async function main() {
     },
   })
   const tag2 = await prisma.tag.upsert({
-    where: { title: tagsData[1].title },
+    where: { title: tagsSeedData[1].title },
     update: {
       authorId: user2.id,
       posts: {
@@ -147,7 +158,7 @@ async function main() {
       },
     },
     create: {
-      title: tagsData[1].title,
+      title: tagsSeedData[1].title,
       authorId: user2.id,
       posts: {
         connect: { id: post2.id },
@@ -155,7 +166,7 @@ async function main() {
     },
   })
   const tag3 = await prisma.tag.upsert({
-    where: { title: tagsData[2].title },
+    where: { title: tagsSeedData[2].title },
     update: {
       authorId: user2.id,
       posts: {
@@ -163,7 +174,7 @@ async function main() {
       },
     },
     create: {
-      title: tagsData[2].title,
+      title: tagsSeedData[2].title,
       authorId: user2.id,
       posts: {
         connect: [{ id: post3.id }, { id: post2.id }],
