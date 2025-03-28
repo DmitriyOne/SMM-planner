a26a8715f8e56779dbc287c1696757c32b4c3256
DmitriyOne <senkoner4@gmail.com>
2025-03-28 20:10:12 +0000

    [frontend] Create entities - post author

---
+++ frontend/src/05_entities/post-author/ui/PostAuthor.tsx
@@ -0,0 +1,32 @@
+import { TUser } from "@/05_entities/user/model"
+import styles from "./post-author.module.scss"
+import { FC } from "react"
+import { formatIsoToDatetime } from "@/06_shared/model/utils"
+import classNames from "classnames"
+
+type TProps = {
+  componentClassName?: string
+  nameClassName?: string
+  dateClassName?: string
+} & Pick<TUser, "name" | "createdAt">
+
+export const PostAuthor: FC<TProps> = ({
+  name,
+  createdAt,
+  componentClassName,
+  nameClassName,
+  dateClassName,
+}) => {
+  const dateString = formatIsoToDatetime(createdAt)
+
+  const componentClass = classNames(styles.component, componentClassName)
+  const nameClass = classNames(styles.username, nameClassName)
+  const dateClass = classNames(styles.date, dateClassName)
+
+  return (
+    <div className={componentClass}>
+      By <span className={nameClass}>{name}</span> at{" "}
+      <span className={dateClass}>{dateString}</span>
+    </div>
+  )
+}
