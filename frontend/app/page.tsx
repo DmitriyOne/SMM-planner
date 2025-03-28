51f06ffec4dcb1fc03cb6d06365aaad970638aed
DmitriyOne <senkoner4@gmail.com>
2025-03-28 20:11:04 +0000

    [frontend] Use post list & update styles

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,5 +1,8 @@
 import { Feed } from "@/02_pages/feed"
+import { getPosts } from "@/03_widgets/post-card-list/api"
 
-export default function Home() {
-  return <Feed />
+export default async function Home() {
+  const posts = await getPosts({})
+
+  return <Feed posts={posts} />
 }
--- frontend/src/02_pages/feed/Feed.tsx
+++ frontend/src/02_pages/feed/Feed.tsx
@@ -1,13 +1,31 @@
 import { Banner } from "@/03_widgets/banner/ui"
 import { PopularTags } from "@/03_widgets/popular-tags/ui"
+import { PostCardList } from "@/03_widgets/post-card-list/ui"
 import { Container } from "@/06_shared/ui/container"
 
-export const Feed = () => {
+import styles from "./feed.module.scss"
+import Title from "antd/es/typography/Title"
+import { TPost } from "@/05_entities/post-card/model"
+import { FC } from "react"
+
+type TProps = {
+  posts: TPost[]
+}
+
+export const Feed: FC<TProps> = ({ posts }) => {
   return (
     <>
       <Banner />
-      <Container>
-        <h1>Feed</h1>
+      <Container className={styles.container}>
+        <div className={styles.feed}>
+          <Title
+            className={styles.title}
+            level={3}
+          >
+            Global Feed
+          </Title>
+          <PostCardList posts={posts} />
+        </div>
         <PopularTags />
       </Container>
     </>
