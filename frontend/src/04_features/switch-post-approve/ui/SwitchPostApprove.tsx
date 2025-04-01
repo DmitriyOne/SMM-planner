2c532cd136338c524368f0dcc1b44b166c20d49b
DmitriyOne <senkoner4@gmail.com>
2025-04-01 08:35:11 +0100

    [frontend] Create feature - switcher approve & publish

---
+++ frontend/src/04_features/switch-post-approve/ui/SwitchPostApprove.tsx
@@ -0,0 +1,32 @@
+"use client"
+
+import { deletePost } from "@/04_features/delete-post/api"
+import { useUpdateData } from "@/06_shared/model/hooks"
+import { Switcher } from "@/06_shared/ui/switcher"
+import { FC, useState } from "react"
+
+type TProps = {
+  defaultValue: boolean
+  className?: string
+}
+
+export const SwitchPostApprove: FC<TProps> = ({ defaultValue, className }) => {
+  const [status, setStatus] = useState(defaultValue)
+  const { isLoading, handleApiCall } = useUpdateData()
+
+  const onChange = async (checked: boolean) => {
+    await handleApiCall({
+      apiFunction: () => deletePost("9"),
+      onSuccessCallback: () => setStatus(checked),
+    })
+  }
+
+  return (
+    <Switcher
+      checked={status}
+      onChange={onChange}
+      loading={isLoading}
+      className={className}
+    />
+  )
+}
