aa20d41b299274de1d46e503c04f9c04061d940c
DmitriyOne <senkoner4@gmail.com>
2025-04-01 08:33:48 +0100

    [frontend] Update description & title as editable

--- frontend/src/05_entities/post-description/ui/PostDescription.tsx
+++ frontend/src/05_entities/post-description/ui/PostDescription.tsx
@@ -1,23 +1,43 @@
 import { TPost } from "@/05_entities/post/model"
-import { FC } from "react"
+import { FC, PropsWithChildren } from "react"
 import Text from "antd/es/typography/Text"
+import classNames from "classnames"
+
+import { editableConfig } from "@/06_shared/config"
 
 import styles from "./post-description.module.scss"
-import classNames from "classnames"
 
 type TProps = Pick<TPost, "description"> & {
   className?: string
-}
+  isEditable?: boolean
+  editableText?: string
+  handleEditable?: (text: string) => void
+  handleEditableSuccess?: () => void
+} & PropsWithChildren
 
-export const PostDescription: FC<TProps> = ({ className, description }) => {
+export const PostDescription: FC<TProps> = ({
+  className,
+  description,
+  isEditable,
+  editableText,
+  handleEditable,
+  handleEditableSuccess,
+  children,
+}) => {
   const componentClassName = classNames(styles.component, className)
+
+  const editable = isEditable
+    ? editableConfig(editableText, handleEditable, handleEditableSuccess)
+    : false
 
   return (
     <Text
       className={componentClassName}
       type='secondary'
+      editable={editable}
     >
-      {description}
+      {isEditable ? editableText : description}
+      {children}
     </Text>
   )
 }
