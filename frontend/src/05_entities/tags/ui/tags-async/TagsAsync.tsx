e80d513b381788a1ca97e836ed1a57e27709aea3
DmitriyOne <senkoner4@gmail.com>
2025-04-13 14:37:12 +0100

    [frontend] Add children + func for icons

--- frontend/src/05_entities/tags/ui/tags-async/TagsAsync.tsx
+++ frontend/src/05_entities/tags/ui/tags-async/TagsAsync.tsx
@@ -1,4 +1,4 @@
-import { FC, use } from "react"
+import { FC, PropsWithChildren, use } from "react"
 import { TTag } from "@/06_shared/ui/tag/model/types"
 import { TagsList } from "../tags-list"
 
@@ -6,12 +6,13 @@ type TProps = {
   getTags: Promise<TTag[]>
   className?: string
   tagClassName?: string
-}
+} & PropsWithChildren
 
 export const TagsAsync: FC<TProps> = ({
   getTags,
   className,
   tagClassName,
+  children,
 }) => {
   const allTags = use(getTags)
 
@@ -20,6 +21,8 @@ export const TagsAsync: FC<TProps> = ({
       tags={allTags}
       className={className}
       tagComponentClassName={tagClassName}
-    />
+    >
+      {children}
+    </TagsList>
   )
 }
--- frontend/src/05_entities/tags/ui/tags-static/TagsStatic.tsx
+++ frontend/src/05_entities/tags/ui/tags-static/TagsStatic.tsx
@@ -1,8 +1,9 @@
-import { FC } from "react"
+import { FC, PropsWithChildren } from "react"
 import { TTag } from "@/06_shared/ui/tag/model/types"
 
 import { EditableTooltip } from "@/06_shared/ui/editable-tooltip"
 import { TagsList } from "../tags-list"
+import { EIconType } from "@/06_shared/config/enums"
 
 type TProps = {
   tags: TTag[]
@@ -11,7 +12,9 @@ type TProps = {
   buttonTooltipClassName?: string
   isEditable?: boolean
   onToggleEdit?: () => void
-}
+  getIconType?: (tag: TTag) => EIconType
+  onTagClick?: (tag: TTag) => void
+} & PropsWithChildren
 
 export const TagsStatic: FC<TProps> = ({
   tags,
@@ -20,13 +23,19 @@ export const TagsStatic: FC<TProps> = ({
   buttonTooltipClassName,
   isEditable,
   onToggleEdit,
+  children,
+  getIconType,
+  onTagClick,
 }) => {
   return (
     <TagsList
       tags={tags}
       className={className}
       tagComponentClassName={tagClassName}
+      getIconType={getIconType}
+      onTagClick={onTagClick}
     >
+      {children}
       {isEditable && (
         <EditableTooltip
           buttonClassName={buttonTooltipClassName}
