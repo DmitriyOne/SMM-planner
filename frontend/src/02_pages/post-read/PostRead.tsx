d611233490c29d4bc8dedef856f9b5d1949a62e9
DmitriyOne <senkoner4@gmail.com>
2025-04-14 17:58:35 +0100

    [frontend] Update page - post read

--- frontend/src/02_pages/post-read/PostRead.tsx
+++ frontend/src/02_pages/post-read/PostRead.tsx
@@ -1,8 +1,22 @@
 import { TPost } from "@/05_entities/post/model"
-import styles from "./post-read.module.scss"
 import { FC } from "react"
 import { Container } from "@/06_shared/ui/container"
-import { PostViewWidget } from "@/03_widgets/post-view/ui"
+import { Col, Row } from "antd"
+import { PostGallery } from "@/05_entities/post-gallery/ui"
+import { TImage } from "@/06_shared/model/types"
+
+import { PostTitle } from "@/05_entities/post-title/ui"
+import { PostDescription } from "@/05_entities/post-description/ui"
+import { PostStatus } from "@/05_entities/post-status/ui"
+import { PostDate } from "@/05_entities/post-date/ui"
+import { PostAuthor } from "@/05_entities/post-author/ui"
+import { DeletePost } from "@/04_features/delete-post/ui"
+import { LinkEdit } from "@/06_shared/ui/link-edit/ui"
+import { paths } from "@/06_shared/config/routing"
+
+import { TagsStatic } from "@/05_entities/tags/ui/tags-static"
+
+import styles from "./post-read.module.scss"
 
 type TProps = {
   post: TPost
@@ -13,9 +27,71 @@ export const PostReadPage: FC<TProps> = ({ post }) => {
     return <></>
   }
 
+  const gallery: TImage[] = [
+    {
+      src: post.image,
+      alt: post.title,
+    },
+  ]
+
+  const postIdString = post.id.toString()
+
   return (
     <Container className={styles.component}>
-      <PostViewWidget post={post} />
+      <Row wrap={false}>
+        <Col
+          flex='400px'
+          className={styles.colLeft}
+        >
+          <PostGallery gallery={gallery} />
+        </Col>
+        <Col
+          flex='auto'
+          className={styles.colRight}
+        >
+          <TagsStatic
+            className={styles.tags}
+            tags={post.tags}
+          />
+          <PostTitle title={post.title} />
+          <PostDescription
+            className={styles.description}
+            description={post.description}
+          />
+          <PostStatus
+            question='Is the post published?'
+            status={post.isPublish}
+          />
+          <PostStatus
+            className={styles.statusApproved}
+            question='Is the post approved?'
+            status={post.isApproved}
+          />
+          <PostAuthor
+            title='Author: '
+            name={post.author?.name}
+          />
+          <PostDate
+            title='Created at:'
+            date={post.createdAt}
+          />
+          <PostDate
+            title='Updated at:'
+            date={post.updatedAt}
+          />
+
+          <div className={styles.bottomButtons}>
+            <DeletePost
+              className={styles.delete}
+              postId={postIdString}
+            />
+            <LinkEdit
+              className={styles.edit}
+              redirectPath={paths.post_edit(postIdString)}
+            />
+          </div>
+        </Col>
+      </Row>
     </Container>
   )
 }
