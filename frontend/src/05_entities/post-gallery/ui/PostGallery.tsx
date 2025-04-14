43fa0392244ffaf52f171de5a040f113eba2ba16
DmitriyOne <senkoner4@gmail.com>
2025-04-14 17:45:48 +0100

    [frontend] Update entities - post gallery

--- frontend/src/05_entities/post-gallery/ui/PostGallery.tsx
+++ frontend/src/05_entities/post-gallery/ui/PostGallery.tsx
@@ -2,21 +2,27 @@ import { TImage } from "@/06_shared/model/types"
 import { FC } from "react"
 import { Image } from "antd"
 
-import styles from "./post-gallery.module.scss"
 import classNames from "classnames"
+import { isValidArray } from "@/06_shared/model/utils"
+
+import styles from "./post-gallery.module.scss"
 
 type TProps = {
   gallery: TImage[]
-  componentClassName?: string
+  className?: string
 }
 
-export const PostGallery: FC<TProps> = ({ gallery, componentClassName }) => {
-  const componentClass = classNames(styles.component, componentClassName)
+export const PostGallery: FC<TProps> = ({ gallery, className }) => {
+  const componentClass = classNames(styles.component, className)
+
+  const imageSrc = isValidArray(gallery)
+    ? gallery[0].src
+    : "/placeholder.jpg"
 
   return (
     <Image
       className={componentClass}
-      src={gallery?.[0]?.src}
+      src={imageSrc}
       alt={gallery?.[0]?.alt}
       width={410}
       height={510}
