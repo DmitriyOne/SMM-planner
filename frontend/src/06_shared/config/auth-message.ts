95d494935f6e7af961fbd6b0952545ae1ca33c77
DmitriyOne <senkoner4@gmail.com>
2025-05-14 20:57:40 +0100

    [frontend] Add share - auth msg

--- frontend/src/06_shared/config/auth-message.ts
+++ frontend/src/06_shared/config/auth-message.ts
@@ -2,3 +2,15 @@ export const AUTH_MESSAGE = {
   INVALID_DATA: (username: string, password: string) =>
     `Invalid basic auth attempt: username="${username}" & password="${password}"`,
 }
+
+export const AUTH_VALIDATE_MSG = {
+  email: {
+    invalid: "Invalid email",
+  },
+  password: {
+    min: "Password must be at least 6 characters long",
+    latin: "Password must contain only Latin alphabet",
+    uppercase: "Password must contain at least two uppercase letters",
+    digits: "Password must contain at least two digits",
+  },
+}
