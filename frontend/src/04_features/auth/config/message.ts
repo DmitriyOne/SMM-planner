e53617135d39b86590eb8a4b054eb30983848dfd
DmitriyOne <senkoner4@gmail.com>
2025-05-16 18:22:07 +0100

    [frontend] Fix auth messages

--- frontend/src/04_features/auth/config/message.ts
+++ frontend/src/04_features/auth/config/message.ts
@@ -1,7 +1,8 @@
 export const AUTH_MESSAGE = {
   success: {
-    login: "Success! You will be redirected to your profile in 3 seconds...",
-    register: "Success! You will be redirected to your profile in 3 seconds...",
+    login: "Logged in successfully",
+    register: "Registration successful",
+    redirectToProfile: "You will be redirected to your profile in 3 seconds...",
   },
   error: {
     login: "Login failed",
