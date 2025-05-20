88a784ee2f915772b3402ba94a86d76501954d04
DmitriyOne <senkoner4@gmail.com>
2025-05-20 11:58:35 +0100

    [frontend] Create generator menu links

--- frontend/src/03_widgets/header/ui/Header.tsx
+++ frontend/src/03_widgets/header/ui/Header.tsx
@@ -1,15 +1,17 @@
 import { Menu } from "@/05_entities/navigation/ui"
 import { Logo } from "@/06_shared/ui"
 
-import styles from "./header.module.scss"
 import { Container } from "@/06_shared/ui/container"
+import { HEADER_MENU_GUEST } from "@/06_shared/config/menu"
+
+import styles from "./header.module.scss"
 
 export const Header = () => {
   return (
     <header className={styles.component}>
       <Container className={styles.wrapper}>
         <Logo />
-        <Menu />
+        <Menu items={HEADER_MENU_GUEST} />
       </Container>
     </header>
   )
