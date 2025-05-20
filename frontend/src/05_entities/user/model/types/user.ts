d54849a13c48dd821ef7285be5456811491738b2
DmitriyOne <senkoner4@gmail.com>
2025-05-20 11:56:27 +0100

    [frontend] Move "role" to shared & create helpers for role

--- frontend/src/05_entities/user/model/types/user.ts
+++ frontend/src/05_entities/user/model/types/user.ts
@@ -1,7 +1,7 @@
 import { TDateFields } from "@/06_shared/api/types"
-import { ERole } from "../enums"
 import { TTag } from "@/06_shared/ui/tag/model/types"
 import { TPost } from "@/05_entities/post/model"
+import { ERole } from "@/06_shared/model/enum"
 
 export type TUser = {
   id: string
