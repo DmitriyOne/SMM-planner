eea7e9c94a659b23da808cd710351c0485bfc33e
DmitriyOne <senkoner4@gmail.com>
2025-05-22 18:11:57 +0100

    [frontend] Update post fields

--- frontend/src/04_features/update-post-description/ui/UpdatePostDescription.tsx
+++ frontend/src/04_features/update-post-description/ui/UpdatePostDescription.tsx
@@ -1,15 +1,14 @@
 "use client"
 
-import { FC, useState } from "react"
-
-import { TPost } from "@/05_entities/post/model"
-import { useUpdateData } from "@/06_shared/model/hooks"
-import { PostDescription } from "@/05_entities/post-description/ui"
-import { LoadingOverlay } from "@/06_shared/ui/loading-overlay"
+import { FC } from "react"
 import classNames from "classnames"
 
+import { TPost } from "@/05_entities/post/model/types"
+import { PostDescription } from "@/05_entities/post-description/ui"
+import { LoadingOverlay } from "@/06_shared/ui/loading-overlay"
+import { useUpdatePostField } from "@/05_entities/post/model/hooks"
+
 import styles from "./update-post-description.module.scss"
-import { updatePost } from "@/05_entities/post/api"
 
 type TProps = Pick<TPost, "id" | "description"> & {
   className?: string
@@ -20,15 +19,8 @@ export const UpdatePostDescription: FC<TProps> = ({
   description,
   className,
 }) => {
-  const [editableText, setEditableText] = useState(description)
-  const { isLoading, handleApiCall } = useUpdateData()
-
-  const handleSuccess = async () => {
-    await handleApiCall({
-      apiFunction: () => updatePost(id, { description: editableText }),
-      onErrorCallback: () => setEditableText(description),
-    })
-  }
+  const { isLoading, newValue, setNewValue, handleSuccess } =
+    useUpdatePostField(id, description, "description")
 
   const componentClassName = classNames(styles.component, className, {
     [styles.loading]: isLoading,
@@ -39,8 +31,8 @@ export const UpdatePostDescription: FC<TProps> = ({
       className={componentClassName}
       description={description}
       isEditable
-      editableText={editableText}
-      handleEditable={setEditableText}
+      editableText={newValue}
+      handleEditable={setNewValue}
       handleEditableSuccess={handleSuccess}
     >
       {isLoading && <LoadingOverlay size='large' />}
