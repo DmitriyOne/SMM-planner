6488a5b62eddf412c284077d12e6152a66025425
DmitriyOne <senkoner4@gmail.com>
2025-05-23 18:59:08 +0100

    [frontend] Check allow to edit

---
+++ frontend/src/05_entities/post/lib/is-user-allow-to-edit-post.ts
@@ -0,0 +1,9 @@
+import { ERole } from "@/06_shared/model/enum"
+
+export const isUserAllowToEditPost = (
+  userRole: ERole | null | undefined,
+  userId: string | null | undefined,
+  postAuthorId: string | null | undefined,
+) => {
+  return userRole === "admin" || userId === postAuthorId
+}
