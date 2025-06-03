38e626ce9565e28b62308eef67f092129f2b2047
DmitriyOne <senkoner4@gmail.com>
2025-06-03 12:56:52 +0100

    [frontend] Test sentry

--- frontend/sentry.server.config.ts
+++ frontend/sentry.server.config.ts
@@ -4,16 +4,12 @@
 
 import * as Sentry from "@sentry/nextjs"
 
-if (process.env.SENTRY_DISABLED === "true") {
-  console.log("Sentry is disabled in development mode")
-} else {
-  Sentry.init({
-    dsn: process.env.SENTRY_DNS,
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
 
-    // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
-    tracesSampleRate: 1,
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
 
-    // Setting this option to true will print useful information to the console while you're setting up Sentry.
-    debug: false,
-  })
-}
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+})
